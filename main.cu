36825af8f04117f45e906fbc9723649e4885d5c7
Leo Forney <forneyleo@gmail.com>
2024-01-28 20:59:23 -0600

    CH2: Output an Image

--- main.cu
+++ main.cu
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int image_height = 256;
+    int image_width = 256;
+
+    std::ofstream fout;
+    fout.open("image.ppm");
+
+    if (!fout.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1; // Or handle the error appropriately
+    }
+
+    fout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            fout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+    fout.close();
     return 0;
 }
