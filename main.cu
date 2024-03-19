76b9ae08cc4604c3ca3b25af063bd3653f5668ff
Leo Forney <forneyleo@gmail.com>
2024-03-19 18:09:39 -0500

    CH9: Diffuse Materials

--- main.cu
+++ main.cu
@@ -25,8 +25,9 @@ int main() {
     camera cam = camera(&fout);
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 1500;
+    cam.image_width  = 400;
     cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
 
     cam.render(world);
 
--- vec3.cuh
+++ vec3.cuh
@@ -20,81 +20,111 @@ public:
     __host__ __device__ double y() const { return e[1]; }
     __host__ __device__ double z() const { return e[2]; }
 
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    double& operator[](int i) { return e[i]; }
+    __host__ __device__ vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    __host__ __device__ double operator[](int i) const { return e[i]; }
+    __host__ __device__ double& operator[](int i) { return e[i]; }
 
-    vec3& operator+=(const vec3 &v) {
+    __host__ __device__ vec3& operator+=(const vec3 &v) {
         e[0] += v.e[0];
         e[1] += v.e[1];
         e[2] += v.e[2];
         return *this;
     }
 
-    vec3& operator*=(double t) {
+    __host__ __device__ vec3& operator*=(double t) {
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(double t) {
+    __host__ __device__ vec3& operator/=(double t) {
         return *this *= 1/t;
     }
 
-    double length() const {
+    __host__ __device__ double length() const {
         return sqrt(length_squared());
     }
 
-    double length_squared() const {
+    __host__ __device__ double length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 };
 
 using point3 = vec3;
 
-inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+__host__ __device__ inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 operator+(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 operator-(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v) {
+__host__ __device__ inline vec3 operator*(double t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, double t) {
+__host__ __device__ inline vec3 operator*(const vec3 &v, double t) {
     return t * v;
 }
 
-inline vec3 operator/(vec3 v, double t) {
+__host__ __device__ inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
 
-inline double dot(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0]
            + u.e[1] * v.e[1]
            + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3 &u, const vec3 &v) {
+__host__ __device__ inline vec3 cross(const vec3 &u, const vec3 &v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v) {
+__host__ __device__ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+__host__ __device__ inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() < 1) {
+            return p;
+        }
+    }
+}
+
+__host__ __device__ inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+__host__ __device__ inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) {
+        return on_unit_sphere;
+    } else {
+        return -on_unit_sphere;
+    }
 }
 
 #endif //RAY_TRACING_IN_ONE_WEEKEND_CUDA_VEC3_CUH
